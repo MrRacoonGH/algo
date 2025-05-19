dd6469933585c218eac9e7a829da3b88096a9295
MrRacoonGH <hadrien181107@gmail.com>
2025-05-19 15:21:13 +0200

    index

--- index.js
+++ index.js
@@ -49,3 +49,76 @@ function estMajuscule(chaine) {
 console.log(estMajuscule("HELLO")); // Résultat attendu : true
 console.log(estMajuscule("Hello")); // Résultat attendu : false
 
+function motsLongueurPaire(tableau) {
+  let result = []
+  for (let i = 0; i < tableau.length; i++) {
+            if (tableau[i].length % 2 === 0) {
+            result.push(tableau[i]);
+        }
+  }
+  return result
+}
+
+console.log(motsLongueurPaire(["chien", "chat", "tortue", "lion", "singe"])); // Output: ["chat", "tortue", "lion"]
+
+
+
+// Exercice 2
+
+function compterNegatifs(tableau) {
+    let compter = 0;
+    for (let i = 0; i < tableau.length; i++) {
+        if (tableau[i] < 0) {
+            compter++;
+        }
+    }
+    return compter;
+}
+
+console.log(compterNegatifs([3, -1, -7, 5, 0, -3])); // Output: 3
+
+// Exercice 3
+
+
+function transformerMajusculesSaufPremier(tableau) {
+    for (let i = 1; i < tableau.length; i++) {
+        tableau[i] = tableau[i].toUpperCase();
+    }
+    return tableau;
+}
+
+console.log(transformerMajusculesSaufPremier(["bonjour", "tout", "le", "monde"])); // Output: ["bonjour", "TOUT", "LE", "MONDE"]
+
+
+// Exercice 4
+
+function supprimerDoublons(tableau) {
+    let resultat = [];
+    for (let i = 0; i < tableau.length; i++) {
+        if (!resultat.includes(tableau[i])) {
+            resultat.push(tableau[i]);
+        }
+    }
+    return resultat;
+}
+
+console.log(supprimerDoublons([1, 2, 3, 2, 4, 1, 5])); 
+
+
+
+// Exercice 5
+
+function filtrerPairs(tableau) {
+    return tableau.filter(nombre => nombre % 2 === 0);
+}
+
+console.log(filtrerPairs([1, 2, 3, 4, 5, 6, 7, 8])); // Output: [2, 4, 6, 8]
+
+
+// Exercice 6
+
+function calculerMoyenne(tableau) {
+}
+
+console.log(calculerMoyenne([10, 20, 30, 40])); // Output: 25
+
